d89b65652fbeaeafb74b0c73d483e836a3cb9a4c
giacomocervi2 <giacomo.cervi.2@studenti.unipd.it>
2026-09-20 21:42:08 +0200

    Delete build directory

--- build/CMakeFiles/LBC.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/LBC"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/LBC.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
